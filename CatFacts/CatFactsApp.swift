43e2c7cd04c9a551bd8a392d92ad2758ba48bc5e
David Lindsay <david_lee_lindsay@yahoo.com>
2023-02-09 15:25:42 -0800

    Initial Commit

---
+++ CatFacts/CatFactsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatFactsApp.swift
+//  CatFacts
+//
+//  Created by David Lindsay on 2/9/23.
+//
+
+import SwiftUI
+
+@main
+struct CatFactsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
